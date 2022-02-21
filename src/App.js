34df8c910451bcbad326d2f8560b90655f103701
kasietfl <chynybekova.k19@gmail.com>
2022-02-21 23:44:29 +0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Card from './components/Card';
+import data from './data';
+
+function App() {
+    const cards = data.map((item) => {
+        return <Card key={item.id} {...item} />;
+    });
+
+    return (
+        <div className='wrapper'>
+            <Navbar />
+            <Hero />
+            <section className='cards-list'>{cards}</section>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+function Navbar() {
+    return (
+        <nav>
+            <img className='nav--img' src='../images/airbnb-logo.png' />
+        </nav>
+    );
+}
+
+export default Navbar;
